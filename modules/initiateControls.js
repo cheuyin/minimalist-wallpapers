7bef88984316d5f2e2e97c30058a3602b0a614eb
cheuyin <yinstanleycheung@gmail.com>
2022-11-06 19:43:27 -0800

    Add character limit to input

--- modules/initiateControls.js
+++ modules/initiateControls.js
@@ -1,26 +1,42 @@
 function initiateControls(canvas) {
+	const CHAR_LIMIT = 30;
+	const mainCanvas = document.querySelector(".main_canvas");
+	const generateWallpaperButton = document.querySelector(".generate button");
+	const phraseInput = document.querySelector(".phrase input");
+	const lightDarkModeButton = document.querySelector(".light-dark-mode button");
+	const characterCounter = document.querySelector(".phrase .character_counter");
+	const fontStyleToggleButton = document.querySelector(".font-style button");
+	const displaySizeOptions = document.querySelector("#display_sizes");
+	const downloadWallpaperButton = document.querySelector(
+		".canvas_container .download_button"
+	);
+
 	function initiateGenerateWallpaperButton() {
-		const phrase = document.querySelector(".phrase input");
-		const generateWallpaperButton = document.querySelector(".generate button");
 		generateWallpaperButton.addEventListener("click", () => {
-			canvas.updateText(phrase.value);
+			canvas.updateText(phraseInput.value);
 			canvas.render();
 		});
 	}
 
-	function initiateInstantPreview() {
-		const phraseInput = document.querySelector(".phrase input");
+	function initiatePhraseInput() {
+		phraseInput.maxLength = CHAR_LIMIT;
 		phraseInput.addEventListener("input", (event) => {
-			canvas.updateText(event.target.value);
-			canvas.render();
+			updateWallpaperPhrase(event);
+			updateCharacterCounter();
+			if (checkForInputPause()) {
+				pauseInput();
+			} else {
+				unpauseInput();
+			}
 		});
+	}
+
+	function updateWallpaperPhrase(event) {
+		canvas.updateText(event.target.value);
+		canvas.render();
 	}
 
 	function initiateLightDarkModeButton() {
-		const lightDarkModeButton = document.querySelector(
-			".light-dark-mode button"
-		);
-
 		lightDarkModeButton.addEventListener("click", () => {
 			canvas.switchMode();
 			canvas.render();
@@ -28,8 +44,6 @@ function initiateControls(canvas) {
 	}
 
 	function initiateFontStyleToggleButton() {
-		const fontStyleToggleButton = document.querySelector(".font-style button");
-
 		fontStyleToggleButton.addEventListener("click", () => {
 			canvas.switchFontStyle();
 			canvas.render();
@@ -37,7 +51,6 @@ function initiateControls(canvas) {
 	}
 
 	function initiateDisplaySizeOptions() {
-		const displaySizeOptions = document.querySelector("#display_sizes");
 		displaySizeOptions.addEventListener("change", (event) => {
 			canvas.changeDisplaySize(event.target.value);
 			canvas.render();
@@ -45,15 +58,17 @@ function initiateControls(canvas) {
 	}
 
 	function initiateDownloadButton() {
-		const downloadWallpaperButton = document.querySelector(
-			".canvas_container .download_button"
-		);
 		downloadWallpaperButton.addEventListener("click", () => {
 			const link = document.createElement("a");
 			link.download = "minipaper.png";
-			link.href = document.querySelector(".main_canvas").toDataURL();
+			link.href = mainCanvas.toDataURL();
 			link.click();
 		});
+	}
+
+	function updateCharacterCounter() {
+		const currentCharacterCount = phraseInput.value.length;
+		characterCounter.textContent = `${currentCharacterCount} / ${CHAR_LIMIT}`;
 	}
 
 	initiateGenerateWallpaperButton();
@@ -61,7 +76,8 @@ function initiateControls(canvas) {
 	initiateFontStyleToggleButton();
 	initiateDisplaySizeOptions();
 	initiateDownloadButton();
-	initiateInstantPreview();
+	initiatePhraseInput();
+	updateCharacterCounter();
 }
 
 export default initiateControls;
